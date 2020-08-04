428c51f4b26fae2da589092cb9e6d3b440879ebd
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-04 23:54:56 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  if board[index] == " " 
+  if board[index] == " "
     false
-  elsif board[index] == "" || board[index] == nil 
+  elsif board[index] == "" || board[index] == nil
     false
   else
     true
