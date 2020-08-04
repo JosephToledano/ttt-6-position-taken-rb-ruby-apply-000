0bc6b4b1ff551d6f21abe667a3085ae4da0b2ba1
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-04 23:53:08 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
-def position_taken(board,index)
+def position_taken?(board,index)
   if board[index] == " "
     false
   elsif board[index] == ""
     false
   else
-    true   
+    true
 end
